707e4a1217dc2ecbab888efac245a9a6168b9561
Johan Naudin <jnaudin@gmail.com>
2021-11-13 14:20:43 +0100

    feature/handles player turn

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,55 @@
 <script lang="ts">
-	export let name: string;
+  export let name: string;
+  let playerColor: "red" | "blue" = "red";
+  function handleClick() {
+    playerColor = playerColor === "red" ? "blue" : "red";
+  }
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Hello {name}!</h1>
+  <p class="instructions">
+    <span class="playerName {playerColor}">{playerColor} player</span>, choose
+    your box.
+  </p>
+  <div on:click={handleClick}>todo boxes</div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .playerName {
+    font-weight: 500;
+  }
+  .instructions {
+    display: block;
+  }
+  .instructions:first-letter {
+    text-transform: capitalize;
+  }
+  .red {
+    color: red;
+  }
+
+  .blue {
+    color: blue;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
