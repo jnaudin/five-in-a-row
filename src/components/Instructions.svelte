21196876d02d2fb55ce19f84de9b99ea1e504e19
Johan Naudin <jnaudin@gmail.com>
2021-11-25 11:23:27 +0100

    adds instructions

--- src/components/Instructions.svelte
+++ src/components/Instructions.svelte
@@ -3,6 +3,10 @@
 </script>
 
 <p class="instructions">
+  The first player to get 5 of her marks in a row (up, down, across, or
+  diagonally) is the winner.
+</p>
+<p class="instructions">
   <span class="playerName" style={`color: ${playerColor}`}
     >{playerColor} player</span
   >, it's your turn to choose a box :
